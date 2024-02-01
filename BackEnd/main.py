cbaff7baff7570dee3f7653f67daa5627dbc37fa
Arnaldo sotomayor <sotomayor85@hotmail.com>
2024-02-01 20:14:00 -0300

    solucion de error 405, agregando el metodo option

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Configuración para permitir solicitudes desde todos los orígenes
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class IPAddress(BaseModel):
     ip: str
 
